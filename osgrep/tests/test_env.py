af808332fd0376dda07f384e613d6613a15282ba
Felipe Reyes <felipe.reyes@canonical.com>
2015-04-04 21:49:43 -0300

    tests: improve test coverage of config sample

--- osgrep/tests/test_env.py
+++ osgrep/tests/test_env.py
@@ -12,4 +12,22 @@ class TestConfig(unittest.TestCase):
         c = env.Config(os.path.join(self.dot, '..', '..',
                                     'config.sample.yaml'))
         (hosts, services) = c.build_env()
-        self.assertEqual(list(hosts.keys()), ['example.com'])
+
+        self.assertEqual(sorted(list(hosts.keys())),
+                         sorted(['example.com', 'localhost']))
+        self.assertEqual(sorted(list(services.keys())),
+                         sorted(['nova-compute', 'nova-api']))
+
+        x = hosts['localhost']
+        self.assertEqual(x.port, 23)
+        self.assertEqual(x.identity_file, 'foobar')
+        self.assertEqual(x.username, 'carrot')
+
+        y = hosts['example.com']
+        self.assertEqual(y.port, 22)
+        self.assertEqual(y.identity_file, '/tmp/id_rsa')
+        self.assertEqual(y.username, 'foo')
+
+        z = services['nova-api']
+        self.assertEqual(sorted([i.server.hostname for i in z]),
+                         ['example.com', 'localhost'])
